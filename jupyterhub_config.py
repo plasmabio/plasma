5b9f3b6b8215df3d41587390cd5140a935598209
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-03-17 18:19:57 +0100

    Use SystemUserSpawner

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,3 +1,21 @@
+import os
+
 from tljh_plasmabio import tljh_custom_jupyterhub_config
 
-tljh_custom_jupyterhub_config(c)  # noqa
+tljh_custom_jupyterhub_config(c)
+
+# configure the volumes
+user_dir = os.path.join(os.getcwd(), "volumes/user/{username}")
+
+
+def create_user_volume(spawner):
+    """
+    A pre-spawn hook to create the user home directory with
+    the correct permissions when testing locally
+    """
+    username = spawner.user.name  # get the username
+    os.makedirs(user_dir.format(username=username), exist_ok=True)
+
+
+c.SystemUserSpawner.pre_spawn_hook = create_user_volume
+c.SystemUserSpawner.host_homedir_format_string = user_dir
