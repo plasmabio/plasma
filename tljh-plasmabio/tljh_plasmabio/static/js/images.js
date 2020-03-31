3202dbc29830a38001e46eb031bf6cc46ff780d8
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-03-31 19:09:43 +0200

    Remove the footer of the spinner modal

--- tljh-plasmabio/tljh_plasmabio/static/js/images.js
+++ tljh-plasmabio/tljh_plasmabio/static/js/images.js
@@ -62,7 +62,9 @@ require(["jquery", "bootstrap", "moment", "jhapi", "utils"], function(
     .click(function() {
       var dialog = $("#remove-image-dialog");
       var image = dialog.find(".delete-image").text();
-      $("#removing-image-dialog").modal();
+      var spinner = $("#removing-image-dialog");
+      spinner.find('.modal-footer').remove();
+      spinner.modal();
       api.api_request("build", {
         type: "DELETE",
         data: JSON.stringify({
