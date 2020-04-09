0fd4a14557a7bafe69876d28a1922564d7d3b4a9
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-04-09 17:47:49 +0200

    Mount read only shared data volume

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -14,8 +14,12 @@ tljh_custom_jupyterhub_config(c)
 
 user = getpass.getuser()
 
-c.Authenticator.admin_users = { user }
+c.Authenticator.admin_users = {user}
 
 volumes_path = os.path.join(os.getcwd(), "volumes/user")
-c.DockerSpawner.volumes = {os.path.join(volumes_path, "{username}"): "/home/jovyan/work"}
+shared_data_path = os.path.join(os.getcwd(), "volumes/data")
+c.DockerSpawner.volumes = {
+    os.path.join(volumes_path, "{username}"): "/home/jovyan/work",
+    shared_data_path: "/home/jovyan/data"
+}
 c.DockerSpawner.pre_spawn_hook = create_pre_spawn_hook(volumes_path, user)
