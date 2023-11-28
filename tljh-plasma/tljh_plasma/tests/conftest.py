2c968ec38885d5d6193eb9dd0992a577818a1b88
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-11-28 09:25:00 +0100

    fix tests

--- tljh-plasma/tljh_plasma/tests/conftest.py
+++ tljh-plasma/tljh_plasma/tests/conftest.py
@@ -1,16 +1,12 @@
-import asyncio
-import os
-import sys
-
 import pytest
 
+from jupyterhub.tests.mocking import MockPAMAuthenticator
 from tljh_repo2docker.tests.conftest import (
     minimal_repo,
     image_name,
     generated_image_name,
     remove_all_test_images
 )
-from tljh_repo2docker.tests.utils import add_environment
 from tljh_repo2docker import tljh_custom_jupyterhub_config as tljh_repo2docker_config
 from tljh_plasma import tljh_custom_jupyterhub_config as tljh_plasma_config
 from traitlets.config import Config
@@ -19,10 +15,11 @@ from traitlets.config import Config
 @pytest.fixture
 async def app(hub_app):
     config = Config()
-    config.authenticator_class = 'jupyterhub.tests.mocking.MockPAMAuthenticator'
 
     tljh_repo2docker_config(config)
     tljh_plasma_config(config)
+
+    config.JupyterHub.authenticator_class = MockPAMAuthenticator
 
     app = await hub_app(config=config)
     return app
